b1c10c8c7b3937a820cb2917e54d78ac36432f64
scyq <chenyuqing0429@foxmail.com>
2022-04-19 22:39:03 +0800

    min data

--- psyqa_processor.py
+++ psyqa_processor.py
@@ -2,7 +2,7 @@ import json
 import re
 import csv
 
-clean_chat_data = "clean_chat_data/qa_data.tsv"
+clean_chat_data = "clean_chat_corpus/qa_data.tsv"
 delimiter = "\t"
 
 
@@ -16,7 +16,8 @@ def json2tsv():
         主要是中英文井号
         所以利用正则表达式只取第一层回复
     '''
-    IF_SPLIT_DATA = False
+    IF_SPLIT_DATA = True
+    IF_DATA_AUGMENTATION = False
     re_splitter = "#|＃|■|※|@|<|>"
 
     # 该数据集涉密, 所以不能直接提取
@@ -28,8 +29,10 @@ def json2tsv():
         for obj in json_data:
             questions = []
             # 记得去除delimiter
-            questions.append(obj["question"].replace(delimiter, ""))
-            questions.append(obj["description"].replace(delimiter, ""))
+            questions.append(obj["question"].replace(delimiter, ""))  # 简洁的问题
+            if IF_DATA_AUGMENTATION:
+                questions.append(obj["description"].replace(
+                    delimiter, ""))  # 问题的描述可以作为另一个问题, 增大训练集
             answers = obj["answers"]
             for question in questions:
                 for answer in answers:
@@ -47,6 +50,7 @@ def json2tsv():
     print("json转tsv完成")
 
 
+# 没写完, 不能用
 def preprocess():
     print("开始预处理...")
     data = []
@@ -54,7 +58,7 @@ def preprocess():
         lines = f.readlines()
         for line in lines:
             sentences = line.strip("\n").split(delimiter)
-            assert (len(sentences) == 2, "每行句子不是两个, 以\t分隔")  # 确保每行有且只有两个句子
+            assert len(sentences) == 2, "每行句子不是两个, 以\t分隔"  # 确保每行有且只有两个句子
             data.append(sentences)
 
 
